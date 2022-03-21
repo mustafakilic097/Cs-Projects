adb127adce32042a31c96a282b1b6d61cccd5646
Mustafa Kılıç <cakirg685@gmail.com>
2022-03-21 15:08:12 +0300

    Maaş Zam Hesaplama
    Maaş ve zam bilgileri girilerek toplam ödenmesi gereken maaşı bulan C# Konsol Uygulaması.

---
+++ Maas-Zam-Hesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HisseDeger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ MAAŞ ZAM HESAPLAMA ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            Console.WriteLine();
+            Console.Write("Maaş: ");
+            double maas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Zam yüzdesi: ");
+            double zam = maas * Convert.ToInt32(Console.ReadLine()) / 100;
+
+
+            Console.WriteLine();
+            Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Maaş: {0} Zam: {1} ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒", maas, zam);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Toplam Maaş: {0}    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒", maas + zam);
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
