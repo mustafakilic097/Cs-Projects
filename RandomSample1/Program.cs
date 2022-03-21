1a6ee4536f7fc2127bd10d56bafdc71deffae735
Mustafa Kılıç <cakirg685@gmail.com>
2022-03-21 22:53:37 +0300

    Random Sample 1
    Random metodu ile yaptığım C# konsol uygulaması.

---
+++ RandomSample1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RandomSample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            double ortalama = 0;
+            Random rnd = new Random();
+            int[] sayilar = new int[20];
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(0, 100);
+                toplam += sayilar[i];
+            }
+            ortalama = toplam / 20;
+            Console.WriteLine("Ortalama: {0}", ortalama);
+            Console.WriteLine();
+            Console.WriteLine("Ortalamadan Küçük Olanlar");
+            foreach (var item in sayilar)
+            {
+                if (item < ortalama)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ortalamadan Büyük Olanlar");
+            foreach (var item in sayilar)
+            {
+                if (item > ortalama)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
