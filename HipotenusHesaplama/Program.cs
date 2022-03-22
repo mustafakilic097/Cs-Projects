72e4a4364ebf49a871ffc300aa91b57da6d44011
Mustafa Kılıç <89447012+mustafakilic097@users.noreply.github.com>
2022-03-22 20:57:41 +0300

    Hipotenus Hesapla (#16)
    Kenar bilgileri verilen bir üçgende hipotenüs açısını hesaplayan C# konsol uygulaması.

---
+++ HipotenusHesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HipotenusHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            Console.WriteLine("▒▒▒▒▒▒Hipotenüs Hesaplama▒▒▒▒▒▒▒▒▒▒▒");
+            Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            string islem = "";
+            while (islem=="")
+            {
+                Console.Write("\n1.Kenar: ");
+                int kenar1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("2.Kenar: ");
+                int kenar2 = Convert.ToInt32(Console.ReadLine());
+
+                double hipotenus = Math.Sqrt((kenar1 * kenar1 + kenar2 * kenar2));
+
+                Console.WriteLine("\n\nHesaplanan Hipotenus: {0}", hipotenus);
+
+
+                Console.WriteLine("\n\nDevam etmek için 'ENTER' basın.");
+                islem = Console.ReadLine();
+            }
+            
+        }
+    }
+}
