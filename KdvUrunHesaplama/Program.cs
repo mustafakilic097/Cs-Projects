85568c32e830a4f7c38b2ac1a0c2a0eb80cc10bc
Mustafa Kılıç <cakirg685@gmail.com>
2022-03-23 13:03:19 +0300

    Kdv ile Toplam Tutar Hesaplama
    Girilen urun fiyatı ve kdv tutar ile urunun tam fiyatını bulan C# konsol uygulaması.

---
+++ KdvUrunHesaplama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KdvUrunHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ürün fiyatı: ");
+            double urunFiyat = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kdv yüzdesi (Örn:18): ");
+            double urunKdv = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            double tutar = urunFiyat + urunFiyat * urunKdv;
+
+            Console.WriteLine("\nÜrün Satış Fiyatı: " + tutar);
+
+
+        }
+    }
+}
