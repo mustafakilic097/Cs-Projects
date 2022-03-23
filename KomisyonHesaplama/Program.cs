a617082b3a1743626aca6148a6b9427952dd53f6
Mustafa Kılıç <cakirg685@gmail.com>
2022-03-23 13:06:51 +0300

    Komisyon Hesaplama
    50 tlye kadar olan urunlerde %3 daha fazlasında ise %2 komisyon alan 5 urun bazında hesaplama yapan C# konsol uygulaması.

---
+++ KomisyonHesaplama/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KomisyonHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double komisyon = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}. ürün fiyatı: ", i + 1);
+                double urunFiyat = Convert.ToDouble(Console.ReadLine());
+
+                if (urunFiyat <= 50)
+                {
+                    komisyon += (urunFiyat * 3 / 100);
+                }
+                else if (urunFiyat > 50)
+                {
+                    komisyon += (urunFiyat * 2 / 100);
+                }
+            }
+
+            Console.WriteLine("Toplam Komisyon Tutarı: " + komisyon);
+            Console.ReadLine();
+        }
+    }
+}
